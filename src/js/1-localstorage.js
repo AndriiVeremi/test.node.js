0d5fafe12be32b6e902df080e91e803d54814676
Andrii Veremii <dashuk10@gmail.com>
2023-03-08 19:30:11 +0200

    add

--- src/js/1-localstorage.js
+++ src/js/1-localstorage.js
@@ -13,7 +13,7 @@
 // // ЛОКАЛ СТОРИДЖ
 // console.log(localStorage)  // цілий час зберегаються дані поки сам не видалтш
 
-// // створює 
+// // створює
 // localStorage.setItem('my-data', 'строка')
 // localStorage.setItem('my-data2', JSON.stringify({ "name": "Mango", "age": 2 }))
 
@@ -24,6 +24,25 @@
 // console.log(localStorage.clearItem('my-data'));
 
 // // СЕШИН СТОРИДЖ  // зберігаються дані тільки коли триває сесія, після перезагрузки сторінки пропадають
-// console.log(sessionStorage) 
+// console.log(sessionStorage)
+
+//=================================================================================
+
+try {
+// код який виконується і може видати помилку
+} catch (error) {
+// якщо винекне помилка виконається тіло кеч  
+}
+// код після конструкції
 
 
+// ========= приклад ==========
+
+
+try {
+    const myJson = JSON.parse('Hi!')
+    //const myJson = JSON.parse("{}")
+} catch (error) {
+    console.log('error.message')  // виведе помилку 
+}
+console.log('success!')
