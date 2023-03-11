6401a84598c36538555528c8d7c77a24a239f87c
Andrii Veremii <dashuk10@gmail.com>
2023-03-11 14:20:18 +0200

    add

--- src/js/1-localstorage.js
+++ src/js/1-localstorage.js
@@ -28,21 +28,75 @@
 
 //=================================================================================
 
-try {
-// код який виконується і може видати помилку
-} catch (error) {
-// якщо винекне помилка виконається тіло кеч  
+// try {
+// // код який виконується і може видати помилку
+// } catch (error) {
+// // якщо винекне помилка виконається тіло кеч
+// }
+// // код після конструкції
+
+
+// // ========= приклад ==========
+
+
+// try {
+//     const myJson = JSON.parse('Hi!')
+//     //const myJson = JSON.parse("{}")
+// } catch (error) {
+//     console.log('error.message')  // виведе помилку
+// }
+// console.log('success!')
+
+
+
+
+
+
+
+
+
+const LOCAL_KEY = 'feedback-form-state';
+let formData = {};
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form  input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('input', throttle(onInputData, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+populateFeedbackForm();
+
+function onInputData(e) {
+    formData = {
+        email: refs.input.value.trim(),
+        message: refs.textarea.value.trim(),
+    };
+    //formData[e.target.name] = e.target.value.trim(); // виводить в localStorage лише один ключ з значенням, якщо інший не заповнений
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
 }
-// код після конструкції
 
+function onFormSubmit(e) {
+    e.preventDefault();
 
-// ========= приклад ==========
+    const { email, message } = e.currentTarget.elements;
+    console.log({ email: email.value.trim(), message: message.value.trim() });
 
-
-try {
-    const myJson = JSON.parse('Hi!')
-    //const myJson = JSON.parse("{}")
-} catch (error) {
-    console.log('error.message')  // виведе помилку 
+    if (localStorage.getItem(LOCAL_KEY)) {
+        // let data = JSON.parse(localStorage.getItem(LOCAL_KEY));
+        // console.log(data);
+        localStorage.removeItem(LOCAL_KEY);
+    }
+    e.currentTarget.reset();
+    formData = {};
 }
-console.log('success!')
+
+function populateFeedbackForm() {
+    let data = localStorage.getItem(LOCAL_KEY);
+    if (!data) return;
+    formData = JSON.parse(data);
+    refs.input.value = formData.email ?? '';
+    refs.textarea.value = formData.message ?? '';
+}
